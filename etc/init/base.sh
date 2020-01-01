bceed5203fd8856592eef531c96adb8ad172e170
arata <arata04962@gmail.com>
2020-01-01 16:04:43 +0900

    :+1: fcitx

--- etc/init/base.sh
+++ etc/init/base.sh
@@ -6,6 +6,6 @@ log_info "Installing base"
 
 sudo pacman --noconfirm -Rs palemoon-bin vlc
 
-sudo pacman --noconfirm -S bat
+sudo pacman --noconfirm -S bat fcitx-im fcitx-mozc
 
 log_success "Installed base"
