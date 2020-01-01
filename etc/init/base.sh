0e7757a3d9d8911181fc21cf884173b1c6c98fa2
arata <arata04962@gmail.com>
2020-01-01 17:07:15 +0900

    :bug: Paths

--- etc/init/base.sh
+++ etc/init/base.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-source ../lib/log.sh
+source etc/lib/log.sh
 
 log_info "Installing base"
 
@@ -8,8 +8,8 @@ sudo pacman --noconfirm -Rs palemoon-bin vlc
 
 sudo pacman --noconfirm -S bat fcitx-im fcitx-mozc i3blocks
 
-yay -S ci ttf-cica
+yay -S ttf-cica
 
-sudo cp -pR ../../assets/fonts/* /usr/share/fonts
+sudo cp -pR assets/fonts/* /usr/share/fonts
 
 log_success "Installed base"
