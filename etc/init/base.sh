96127f4ea9d6363d37114423c4080a8625523672
arata <arata04962@gmail.com>
2020-01-01 17:47:45 +0900

    :+1: Some applications

--- etc/init/base.sh
+++ etc/init/base.sh
@@ -6,9 +6,9 @@ log_info "Installing base"
 
 sudo pacman --noconfirm -Rs palemoon-bin vlc
 
-sudo pacman --noconfirm -S bat fcitx-im fcitx-mozc i3blocks
+sudo pacman --noconfirm -S bat fcitx-im fcitx-mozc i3blocks mpv rofi skippy-xd redshift firefox-developer-edition
 
-yay -S ttf-cica
+yay -S ttf-cica ruby-fusuma
 
 sudo cp -pR assets/fonts/* /usr/share/fonts
 sudo cp -pR assets/wallpapers/* /usr/share/backgrounds
