608ceb0077de27601fdd9502561462cc114c9b82
arata <arata04962@gmail.com>
2020-05-31 17:05:27 +0900

    :+1: fish

--- home/.config/fish/config.fish
+++ home/.config/fish/config.fish
@@ -1,10 +1,8 @@
-set -x PATH $HOME/.anyenv/bin ~/.node/bin $PATH
+set -x PATH $HOME/.anyenv/bin ~/.node/bin ~/.gem/ruby/2.7.0/bin $PATH
+set -x FZF_DEFAULT_COMMAND 'rg --files --hidden --glob "!.git"'
+set -x FZF_DEFAULT_OPTS '--height 30% --border'
 eval anyenv init - | source
 
 alias htb='sudo openvpn $HOME/vpn/htb-aratanvm.ovpn'
 alias thm='sudo openvpn $HOME/vpn/thm-aratanvm.ovpn'
-
-fusuma 1> /dev/null 2> /dev/null &
-
-eval (opam env)
 
